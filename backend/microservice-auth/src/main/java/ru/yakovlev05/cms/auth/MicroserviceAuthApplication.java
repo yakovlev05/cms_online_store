102db09b65c32ede18f6951e0e18b82dc01ded72
yakovlev05 <alexey.yakovlev.05@mail.ru>
2024-10-22 20:10:44 +0500

    init

---
+++ backend/microservice-auth/src/main/java/ru/yakovlev05/cms/auth/MicroserviceAuthApplication.java
@@ -0,0 +1,13 @@
+package ru.yakovlev05.cms.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceAuthApplication.class, args);
+    }
+
+}
