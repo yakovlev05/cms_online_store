f8714d48151d57039d5f9e360efdfeaac8b076f4
yakovlev05 <alexey.yakovlev.05@mail.ru>
2024-11-02 19:36:04 +0500

    add entities

---
+++ backend/microservice-user/src/main/java/ru/yakovlev05/cms/user/microserviceuser/entity/Role.java
@@ -0,0 +1,29 @@
+package ru.yakovlev05.cms.user.microserviceuser.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+@Entity
+@Table(name = "role")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "name", nullable = false, unique = true, length = 50)
+    private UserRole name;
+
+    @ManyToMany(mappedBy = "roles", fetch = FetchType.LAZY)
+    private List<User> users;
+}
---
+++ backend/microservice-user/src/main/java/ru/yakovlev05/cms/user/microserviceuser/entity/UserRole.java
@@ -0,0 +1,9 @@
+package ru.yakovlev05.cms.user.microserviceuser.entity;
+
+public enum UserRole {
+    ROLE_OWNER,
+    ROLE_ADMIN,
+    ROLE_MODER,
+    ROLE_SUPPORT,
+    ROLE_CUSTOMER
+}
