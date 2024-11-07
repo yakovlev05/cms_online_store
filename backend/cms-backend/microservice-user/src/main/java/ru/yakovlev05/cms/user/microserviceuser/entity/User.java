ea1ecd5057c61e1cfc705325255021ad1ccc695e
yakovlev05 <alexey.yakovlev.05@mail.ru>
2024-11-07 20:38:23 +0500

    update user entity in user service

--- backend/cms-backend/microservice-user/src/main/java/ru/yakovlev05/cms/user/microserviceuser/entity/User.java
+++ backend/cms-backend/microservice-user/src/main/java/ru/yakovlev05/cms/user/microserviceuser/entity/User.java
@@ -20,14 +20,17 @@ public class User {
     @Id
     private long id;
 
-    @Column(name = "username", nullable = false, unique = true, length = 25)
-    private String username;
+    @Column(name = "phone_number", nullable = false, unique = true, length = 10)
+    private String phoneNumber;
 
-    @Column(name = "email", nullable = false, unique = true, length = 100)
-    private String email;
+    @Column(name = "first_name")
+    private String firstName;
 
-    @Column(name = "password", nullable = false, length = 100)
-    private String password;
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "patronymic")
+    private String patronymic;
 
     @Column(name = "created_at", nullable = false)
     private LocalDateTime createdAt;
