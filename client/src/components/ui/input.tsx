f8e1da9b97581134ad700a0b7ce9729538197c7a
yakovlev05 <alexey.yakovlev.05@mail.ru>
2024-11-16 14:04:42 +0500

    add input

---
+++ client/src/components/ui/input.tsx
@@ -0,0 +1,22 @@
+import styles from '@/src/styles/input.module.css';
+import React from "react";
+
+interface Props {
+    label: string;
+    placeholder: string;
+    inputName: string;
+    inputAutoComplete: string;
+    inputType: string;
+}
+
+const Input: React.FC<Props> = ({label, placeholder = label, inputName, inputAutoComplete, inputType}) => {
+    return (
+        <div>
+            <p className={styles.label}>{label}</p>
+            <input placeholder={placeholder} type={inputType} autoComplete={inputAutoComplete} name={inputName}
+                   className={styles.input}/>
+        </div>
+    )
+}
+
+export default Input;
