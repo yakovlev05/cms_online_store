1c2022fdc144189d07153c654f4efba5ddee82e6
yakovlev05 <alexey.yakovlev.05@mail.ru>
2024-11-18 17:55:43 +0500

    fix lombok dependency

--- backend/cms-backend/microservice-catalog/build.gradle.kts
+++ backend/cms-backend/microservice-catalog/build.gradle.kts
@@ -29,13 +29,14 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.liquibase:liquibase-core")
-    implementation("org.projectlombok:lombok")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("io.jsonwebtoken:jjwt-api:$jjwtVersion") // jwt
     implementation(project(":core"))
+    implementation("org.projectlombok:lombok")
     runtimeOnly("org.postgresql:postgresql")
     runtimeOnly("io.jsonwebtoken:jjwt-impl:$jjwtVersion") //jwt
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:$jjwtVersion") // jwt
+    annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.security:spring-security-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
