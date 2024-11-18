a08798ae2db60b5a616a6a158a31145307afab27
yakovlev05 <alexey.yakovlev.05@mail.ru>
2024-11-18 15:38:47 +0500

    create entities

---
+++ backend/cms-backend/microservice-catalog/src/main/java/ru/yakovlev05/cms/catalog/entity/Media.java
@@ -0,0 +1,31 @@
+package ru.yakovlev05.cms.catalog.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "media")
+public class Media {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "file_name", nullable = false, unique = true)
+    private String fileName;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+}
