b00ef2126f7bc2ab1980a00216cdd7fe2c8d6d2a
yakovlev05 <alexey.yakovlev.05@mail.ru>
2024-11-22 01:11:38 +0500

    update docker minio

--- backend/cms-backend/microservice-catalog/src/main/java/ru/yakovlev05/cms/catalog/config/S3Config.java
+++ backend/cms-backend/microservice-catalog/src/main/java/ru/yakovlev05/cms/catalog/config/S3Config.java
@@ -8,6 +8,7 @@ import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.S3Configuration;
 
 import java.net.URI;
 
@@ -29,6 +30,9 @@ public class S3Config {
 
                 ))
                 .endpointOverride(URI.create(s3Properties.getUrl()))
+//                .serviceConfiguration(S3Configuration.builder()
+//                        .pathStyleAccessEnabled(true) // Включить Path-Style
+//                        .build())
                 .build();
     }
 }
