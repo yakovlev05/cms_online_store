6072c0970153416a2649b4b63ecf91fbf16692be
yakovlev05 <alexey.yakovlev.05@mail.ru>
2024-11-23 23:06:47 +0500

    add handle S3 Exception

--- backend/cms-backend/microservice-catalog/src/main/java/ru/yakovlev05/cms/catalog/exception/handler/GlobalExceptionHandler.java
+++ backend/cms-backend/microservice-catalog/src/main/java/ru/yakovlev05/cms/catalog/exception/handler/GlobalExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ru.yakovlev05.cms.catalog.dto.ExceptionDto;
 import ru.yakovlev05.cms.catalog.exception.BadRequestException;
+import ru.yakovlev05.cms.catalog.exception.S3Exception;
 
 @Slf4j
 @RestControllerAdvice
@@ -19,5 +20,12 @@ public class GlobalExceptionHandler {
         ExceptionDto exDto = new ExceptionDto(e.getMessage());
         exDto.setErrors(e.getErrors());
         return exDto;
+    }
+
+    @ExceptionHandler(S3Exception.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ExceptionDto handleS3Exception(S3Exception e) {
+        log.info("Handle S3Exception: {}", e.getMessage());
+        return new ExceptionDto(e.getMessage());
     }
 }
