115374389cee9affabcc70a550c1f39b78c33093
yakovlev05 <alexey.yakovlev.05@mail.ru>
2024-11-23 14:23:20 +0500

    add Collection entity

---
+++ backend/cms-backend/microservice-catalog/src/main/java/ru/yakovlev05/cms/catalog/entity/Collection.java
@@ -0,0 +1,27 @@
+package ru.yakovlev05.cms.catalog.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "collection")
+public class Collection {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @OneToOne
+    @JoinColumn(name = "category_id", referencedColumnName = "id")
+    private Category category;
+
+    @OneToOne
+    @JoinColumn(name = "photo_id", referencedColumnName = "id")
+    private Media photo;
+}
