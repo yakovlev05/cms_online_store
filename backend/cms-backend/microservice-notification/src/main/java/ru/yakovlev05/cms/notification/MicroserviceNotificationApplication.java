ab50c7bc0181d17d32f6c940b833579ae2962b94
yakovlev05 <alexey.yakovlev.05@mail.ru>
2024-12-09 19:48:03 +0500

    disable userDetails and add RestTemplateConfig

--- backend/cms-backend/microservice-notification/src/main/java/ru/yakovlev05/cms/notification/MicroserviceNotificationApplication.java
+++ backend/cms-backend/microservice-notification/src/main/java/ru/yakovlev05/cms/notification/MicroserviceNotificationApplication.java
@@ -2,8 +2,9 @@ package ru.yakovlev05.cms.notification;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication(scanBasePackages = "ru.yakovlev05.cms")
+@SpringBootApplication(scanBasePackages = "ru.yakovlev05.cms", exclude = {UserDetailsServiceAutoConfiguration.class})
 public class MicroserviceNotificationApplication {
 
     public static void main(String[] args) {
