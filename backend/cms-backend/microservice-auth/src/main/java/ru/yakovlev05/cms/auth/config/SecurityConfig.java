97a1b79a887a8fe320b9b0e653d82c957079b652
yakovlev05 <alexey.yakovlev.05@mail.ru>
2024-12-09 15:09:49 +0500

    update SecurityConfig

--- backend/cms-backend/microservice-auth/src/main/java/ru/yakovlev05/cms/auth/config/SecurityConfig.java
+++ backend/cms-backend/microservice-auth/src/main/java/ru/yakovlev05/cms/auth/config/SecurityConfig.java
@@ -39,6 +39,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(request -> request
                         .requestMatchers("/api/v1/auth/**").permitAll()
                         .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                        .requestMatchers("/api/v1/otp/**").permitAll()
                         .anyRequest().authenticated())
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
