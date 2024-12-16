cd3784f66c421266473b84f13a372c8c1a128ec7
yakovlev05 <alexey.yakovlev.05@mail.ru>
2024-12-16 17:04:23 +0500

    init microservice-order project

---
+++ backend/cms-backend/microservice-order/src/main/java/ru/yakovlev05/cms/order/MicroserviceOrderApplication.java
@@ -0,0 +1,13 @@
+package ru.yakovlev05.cms.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceOrderApplication.class, args);
+    }
+
+}
