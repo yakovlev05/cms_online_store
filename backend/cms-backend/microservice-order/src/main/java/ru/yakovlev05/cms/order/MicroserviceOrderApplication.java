7a7f385277e373f41dab76462b5db96c73d77a17
yakovlev05 <alexey.yakovlev.05@mail.ru>
2024-12-20 18:25:08 +0500

    add SecurityConfig

--- backend/cms-backend/microservice-order/src/main/java/ru/yakovlev05/cms/order/MicroserviceOrderApplication.java
+++ backend/cms-backend/microservice-order/src/main/java/ru/yakovlev05/cms/order/MicroserviceOrderApplication.java
@@ -3,7 +3,7 @@ package ru.yakovlev05.cms.order;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "ru.yakovlev05.cms")
 public class MicroserviceOrderApplication {
 
     public static void main(String[] args) {
