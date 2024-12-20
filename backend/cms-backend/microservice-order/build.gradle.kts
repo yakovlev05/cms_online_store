94c69433fd222b5d92c74b3916c5d00501efed18
yakovlev05 <alexey.yakovlev.05@mail.ru>
2024-12-20 21:29:47 +0500

    add swagger dependency

--- backend/cms-backend/microservice-order/build.gradle.kts
+++ backend/cms-backend/microservice-order/build.gradle.kts
@@ -23,11 +23,14 @@ repositories {
     mavenCentral()
 }
 
+val swaggerVersion = "2.6.0"
+
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.kafka:spring-kafka")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:${swaggerVersion}")
     implementation(project(":core"))
     implementation("org.liquibase:liquibase-core")
     compileOnly("org.projectlombok:lombok")
