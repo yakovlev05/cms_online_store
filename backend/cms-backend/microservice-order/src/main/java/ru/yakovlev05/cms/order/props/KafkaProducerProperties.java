1ee25f5267f4f071abfa5fe8fec318dca7e73c9b
yakovlev05 <alexey.yakovlev.05@mail.ru>
2024-12-20 21:28:18 +0500

    fix KafkaProducerProperties

--- backend/cms-backend/microservice-order/src/main/java/ru/yakovlev05/cms/order/props/KafkaProducerProperties.java
+++ backend/cms-backend/microservice-order/src/main/java/ru/yakovlev05/cms/order/props/KafkaProducerProperties.java
@@ -1,6 +1,7 @@
 package ru.yakovlev05.cms.order.props;
 
 import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
@@ -9,8 +10,13 @@ import org.springframework.stereotype.Component;
 @ConfigurationProperties(prefix = "spring.kafka")
 public class KafkaProducerProperties {
     private String bootstrapServers;
+
+    @Value("${spring.kafka.producer.key-serializer}")
     private String producerKeySerializer;
+
+    @Value("${spring.kafka.producer.value-serializer}")
     private String producerValueSerializer;
+
     private String orderValidationInputTopicName;
     private int orderValidationInputTopicPartitions;
     private int orderValidationInputTopicReplicas;
