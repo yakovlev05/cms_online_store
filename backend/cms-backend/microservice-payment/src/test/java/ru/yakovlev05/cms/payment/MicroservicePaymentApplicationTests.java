502607d1127fd4565351aa85fd26ef398a600687
yakovlev05 <alexey.yakovlev.05@mail.ru>
2024-12-21 17:57:25 +0500

    init project

---
+++ backend/cms-backend/microservice-payment/src/test/java/ru/yakovlev05/cms/payment/MicroservicePaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.yakovlev05.cms.payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicePaymentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
