38a5789ab9fc32d0313d34ff5923c6f234b0c891
yakovlev05 <alexey.yakovlev.05@mail.ru>
2024-12-24 00:16:52 +0500

    fix MenuComponent.tsx

--- admin/src/components/MenuComponent.tsx
+++ admin/src/components/MenuComponent.tsx
@@ -1,5 +1,4 @@
-import {useState} from 'react';
-import {MenuFoldOutlined, MenuUnfoldOutlined, ShoppingCartOutlined} from '@ant-design/icons';
+import {ShoppingCartOutlined} from '@ant-design/icons';
 import {Button, Menu} from 'antd';
 import * as React from "react";
 import {SelectInfo} from "rc-menu/lib/interface";
@@ -7,7 +6,7 @@ import {useNavigate} from "react-router";
 
 
 const MenuComponent: React.FC = () => {
-    const [collapsed, setCollapsed] = useState<boolean>(false);
+    // const [collapsed, setCollapsed] = useState<boolean>(false);
     const navigate = useNavigate();
 
     const handleSelect = (info: SelectInfo) => {
@@ -19,17 +18,18 @@ const MenuComponent: React.FC = () => {
         <div style={{width: '256px', minHeight: '100vh', display: 'flex', flexDirection: 'column'}}>
             <Button
                 type="primary"
-                onClick={() => setCollapsed(!collapsed)}
+                // onClick={() => setCollapsed(!collapsed)}
                 style={{width: '100%'}}>
-                {collapsed ? <MenuUnfoldOutlined/> : <MenuFoldOutlined/>}
-                {collapsed ? "Развернуть" : "Свернуть"}
+                {/*{collapsed ? <MenuUnfoldOutlined/> : <MenuFoldOutlined/>}*/}
+                {/*{collapsed ? "Развернуть" : "Свернуть"}*/}
+                Панель управления
             </Button>
             <Menu
                 style={{flex: '1'}}
                 defaultSelectedKeys={window.location.pathname.split('/')}
                 defaultOpenKeys={window.location.pathname.split('/')}
                 mode="inline"
-                inlineCollapsed={collapsed}
+                // inlineCollapsed={collapsed}
                 items={[
                     {
                         key: 'catalog', label: 'Каталог', icon: <ShoppingCartOutlined/>,
