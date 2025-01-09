e7f4c408f56f6805086789b05c3eed64911dad1a
yakovlev05 <alexey.yakovlev.05@mail.ru>
2025-01-09 19:33:33 +0500

    add backend request for login page

--- client/src/components/ui/input.tsx
+++ client/src/components/ui/input.tsx
@@ -7,14 +7,15 @@ interface Props {
     inputName: string;
     inputAutoComplete: string;
     inputType: string;
+    onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-const Input: React.FC<Props> = ({label, placeholder = label, inputName, inputAutoComplete, inputType}) => {
+const Input: React.FC<Props> = ({label, placeholder = label, inputName, inputAutoComplete, inputType, onChange}) => {
     return (
         <div>
             <p className={styles.label}>{label}</p>
             <input placeholder={placeholder} type={inputType} autoComplete={inputAutoComplete} name={inputName}
-                   className={styles.input}/>
+                   className={styles.input} onChange={e => onChange && onChange(e)}/>
         </div>
     )
 }
