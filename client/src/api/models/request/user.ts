0f8c57664858c0575a9585ff9d72f392b88d4550
Yury <yurysorokin05@gmail.com>
2025-01-12 00:04:44 +0500

    Добавил модели запросов

---
+++ client/src/api/models/request/user.ts
@@ -0,0 +1,11 @@
+export interface UserRequestDto {
+    userId: string;
+}
+
+export interface UpdateUserRequestDto {
+    fistName: string;
+    lastName: string;
+    patronymic: string;
+    phoneNumber: string;
+    password: string;
+}
---
+++ client/src/api/models/response/user.ts
@@ -0,0 +1,8 @@
+export interface UserResponseDto {
+    id: string;
+    firstName: string;
+    lastName: string;
+    patronymic: string;
+    phoneNumber: string;
+    address: string;
+}
