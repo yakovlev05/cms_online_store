1c267cfe69c5d2fc7f169011777bdc2d7c50da90
Yury <yurysorokin05@gmail.com>
2025-01-12 11:11:30 +0500

    _

--- client/src/api/models/response/user.ts
+++ client/src/api/models/response/user.ts
@@ -1,8 +1,8 @@
 export interface UserResponseDto {
     id: string;
-    firstName: string;
-    lastName: string;
-    patronymic: string;
+    firstName?: string;
+    lastName?: string;
+    patronymic?: string;
     phoneNumber: string;
-    address: string;
+    address?: string;
 }
