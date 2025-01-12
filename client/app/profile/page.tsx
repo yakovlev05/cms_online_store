24847d3c9f5c0fc109f7d0d5390149bfc96c6b49
Yury <yurysorokin05@gmail.com>
2025-01-13 00:15:11 +0500

    added profile styles

---
+++ client/app/profile/page.tsx
@@ -0,0 +1,21 @@
+import Header from '@/src/components/header';
+import UserProfile from '@/src/components/profile';
+import OrderHistory from '@/src/components/order-history';
+import Footer from '@/src/components/footer';
+import styles from '@/src/styles/profile.module.css'
+
+export default function Profile() {
+    return (
+        <div className={styles.mainContainer}>
+            <Header logo='/assets/placeholder/logo.svg'/>
+            <div className={styles.profileContainer}>
+                <UserProfile userId=""/>
+                <OrderHistory/>
+            </div>
+            <Footer logo='/assets/placeholder/logo.svg'
+                    vk_link='/#'
+                    tg_link='/#'
+            />
+        </div>
+    )
+}
